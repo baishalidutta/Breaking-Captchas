1de987f168a87aa9773d736f1f0e35b96c58e0d1
Baishali Dutta <me@itsbaishali.com>
2021-08-28 10:50:56 +0200

    Added Copyright

--- train_model.py
+++ train_model.py
@@ -1,3 +1,8 @@
+__author__ = "Baishali Dutta"
+__copyright__ = "Copyright (C) 2021 Baishali Dutta"
+__license__ = "Apache License 2.0"
+__version__ = "0.1"
+
 # USAGE
 # python train_model.py --dataset dataset --model output/lenet.hdf5
 
