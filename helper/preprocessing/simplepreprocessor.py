532a2e930f6ec1ee7c5ead67bff478e34b2e308e
Baishali Dutta <me@itsbaishali.com>
2022-02-25 08:30:03 +0100

    Reformatted Simple Preprocessor

--- helper/preprocessing/simplepreprocessor.py
+++ helper/preprocessing/simplepreprocessor.py
@@ -3,19 +3,19 @@ __copyright__ = "Copyright (C) 2022 Baishali Dutta"
 __license__ = "Apache License 2.0"
 __version__ = "0.1"
 
-# import the necessary packages
 import cv2
 
-class SimplePreprocessor:
-	def __init__(self, width, height, inter=cv2.INTER_AREA):
-		# store the target image width, height, and interpolation
-		# method used when resizing
-		self.width = width
-		self.height = height
-		self.inter = inter
 
-	def preprocess(self, image):
-		# resize the image to a fixed size, ignoring the aspect
-		# ratio
-		return cv2.resize(image, (self.width, self.height),
-			interpolation=self.inter)
+class SimplePreprocessor:
+    def __init__(self, width, height, inter=cv2.INTER_AREA):
+        # store the target image width, height, and interpolation
+        # method used when resizing
+        self.width = width
+        self.height = height
+        self.inter = inter
+
+    def preprocess(self, image):
+        # resize the image to a fixed size, ignoring the aspect
+        # ratio
+        return cv2.resize(image, (self.width, self.height),
+                          interpolation=self.inter)
