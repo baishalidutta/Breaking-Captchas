8af6c0f0176b97bda54b29120cc92895b95b1f4d
Baishali Dutta <me@itsbaishali.com>
2022-06-05 08:29:03 +0200

    Fixed variable names in shallow network

--- helper/nn/conv/shallownet.py
+++ helper/nn/conv/shallownet.py
@@ -17,15 +17,15 @@ class ShallowNet:
 		# initialize the model along with the input shape to be
 		# "channels last"
 		model = Sequential()
-		inputShape = (height, width, depth)
+		input_shape = (height, width, depth)
 
 		# if we are using "channels first", update the input shape
 		if K.image_data_format() == "channels_first":
-			inputShape = (depth, height, width)
+			input_shape = (depth, height, width)
 
 		# define the first (and only) CONV => RELU layer
 		model.add(Conv2D(32, (3, 3), padding="same",
-			input_shape=inputShape))
+			input_shape=input_shape))
 		model.add(Activation("relu"))
 
 		# softmax classifier
