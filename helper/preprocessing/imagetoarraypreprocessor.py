1398abd8c59b9f685a15397b5026acd583448ca5
Baishali Dutta <me@itsbaishali.com>
2022-06-06 08:29:37 +0200

    Fixed argument var names in mean preprocessor

--- helper/preprocessing/imagetoarraypreprocessor.py
+++ helper/preprocessing/imagetoarraypreprocessor.py
@@ -8,11 +8,11 @@ from tensorflow.keras.preprocessing.image import img_to_array
 
 
 class ImageToArrayPreprocessor:
-    def __init__(self, dataFormat=None):
+    def __init__(self, data_format=None):
         # store the image data format
-        self.dataFormat = dataFormat
+        self.data_format = data_format
 
     def preprocess(self, image):
         # apply the Keras utility function that correctly rearranges
         # the dimensions of the image
-        return img_to_array(image, data_format=self.dataFormat)
+        return img_to_array(image, data_format=self.data_format)
