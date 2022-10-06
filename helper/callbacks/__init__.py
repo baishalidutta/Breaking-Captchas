cc599ba58b0ad3389b9e0b71b2be0434c8cd1fe5
Baishali Dutta <me@itsbaishali.com>
2022-10-06 10:44:06 +0200

    Removed unnecessary import from callbacks module

--- helper/callbacks/__init__.py
+++ helper/callbacks/__init__.py
@@ -2,6 +2,3 @@ __author__ = "Baishali Dutta"
 __copyright__ = "Copyright (C) 2021-2022 Baishali Dutta"
 __license__ = "Apache License 2.0"
 __version__ = "0.1"
-
-# import the necessary packages
-from .trainingmonitor import TrainingMonitor
