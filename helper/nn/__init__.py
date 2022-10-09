24664863910985dcf4787088288d2c6044d26eac
Baishali Dutta <me@itsbaishali.com>
2022-10-09 10:45:01 +0200

    Removed unnecessary import from nn module

--- helper/nn/__init__.py
+++ helper/nn/__init__.py
@@ -2,7 +2,3 @@ __author__ = "Baishali Dutta"
 __copyright__ = "Copyright (C) 2021-2022 Baishali Dutta"
 __license__ = "Apache License 2.0"
 __version__ = "0.1"
-
-# import the necessary packages
-from .neuralnetwork import NeuralNetwork
-from .perceptron import Perceptron
