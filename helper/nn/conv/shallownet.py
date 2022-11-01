249dfd8171d48464e0807884ee3117263606fa7a
Baishali Dutta <me@itsbaishali.com>
2022-11-01 22:18:38 +0100

    Reformatted shallow net

--- helper/nn/conv/shallownet.py
+++ helper/nn/conv/shallownet.py
@@ -11,27 +11,28 @@ from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
 from tensorflow.keras import backend as K
 
+
 class ShallowNet:
-	@staticmethod
-	def build(width, height, depth, classes):
-		# initialize the model along with the input shape to be
-		# "channels last"
-		model = Sequential()
-		input_shape = (height, width, depth)
+    @staticmethod
+    def build(width, height, depth, classes):
+        # initialize the model along with the input shape to be
+        # "channels last"
+        model = Sequential()
+        input_shape = (height, width, depth)
 
-		# if we are using "channels first", update the input shape
-		if K.image_data_format() == "channels_first":
-			input_shape = (depth, height, width)
+        # if we are using "channels first", update the input shape
+        if K.image_data_format() == "channels_first":
+            input_shape = (depth, height, width)
 
-		# define the first (and only) CONV => RELU layer
-		model.add(Conv2D(32, (3, 3), padding="same",
-			input_shape=input_shape))
-		model.add(Activation("relu"))
+        # define the first (and only) CONV => RELU layer
+        model.add(Conv2D(32, (3, 3), padding="same",
+                         input_shape=input_shape))
+        model.add(Activation("relu"))
 
-		# softmax classifier
-		model.add(Flatten())
-		model.add(Dense(classes))
-		model.add(Activation("softmax"))
+        # softmax classifier
+        model.add(Flatten())
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
 
-		# return the constructed network architecture
-		return model
+        # return the constructed network architecture
+        return model
